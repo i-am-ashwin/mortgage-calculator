c5ab24477b73af4469b3bd5ba609539b970a0199
i-am-ashwin <aswin.harikumar@yahoo.com>
2025-08-31 23:25:59 +0200

    feat: result template

--- src/components/MortgageResult.vue
+++ src/components/MortgageResult.vue
@@ -1,5 +1,27 @@
 <template>
-    <div class="">
-        Hi I am result
+  <div class="bg-white rounded-2xl shadow-xl p-8 mt-8 border border-gray-100">
+    <div class="space-y-6">
+      <h2 class="text-2xl font-semibold text-gray-900">Results</h2>
+      <div class="rounded-xl p-6 border border-teal-200">
+        <div class="flex items-center justify-between">
+          <div>
+            <h3 class="text-lg font-medium text-gray-900 mb-1">Implied Loan</h3>
+          </div>
+          <div class="text-right">
+            <div class="text-3xl font-bold text-teal-700">€ 1000</div>
+          </div>
+        </div>
+      </div>
+            <div class="rounded-xl p-6 border border-teal-200">
+        <div class="flex items-center justify-between">
+          <div>
+            <h3 class="text-lg font-medium text-gray-900 mb-1">Loan to value</h3>
+          </div>
+          <div class="text-right">
+            <div class="text-3xl font-bold text-blue-700">99 %</div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
